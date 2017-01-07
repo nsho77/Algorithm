452f2b23f8dbf3688c282187bb0c821d83678e8c
seongho Roh <nsho717@gmail.com>
2017-01-07 17:52:18 +0900

    Signed-off-by: seongho Roh <nsho717@gmail.com>

--- Algorithm/src/finda/Jsonparser.java
+++ Algorithm/src/finda/Jsonparser.java
@@ -1,17 +1,29 @@
 package finda;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Jsonparser {
+	String sample = "[{name: 'Douglas Crawford', salary:80000, address: {line1: 'c/o Unstructured Programmers Anonymous', line2: '7723 Western Avenue', city: 'San Francisco', state: 'CA', zip: '9123'}},"
+			+ "{name: 'Brian Kernighan', salary:60000, address: {line1: 'PO BOX 455', city:'Princeton', state:'NJ', zip: '08544'}}]";
+
+	String sample2 = "[{name: \"Douglas Crawford\", salary:80000, address: {line1: \"c/o Unstructured Programmers Anonymous\", line2: \"7723 Western Avenue\", city: \"San Francisco\", state: \"CA\", zip: \"9123\"}},"
+			+ "{name: \"Brian Kernighan\", salary:60000, address: {line1: \"PO BOX 455\", city:\"Princeton\", state:\"NJ\", zip: \"08544\"}}]";
+
+	String [] first = sample2.split(",");
+	Map<String , String> map1 = new HashMap<String,String>();
+
 	public static void main(String[] args) {
-
-		String sample = "[{name: 'Douglas Crawford', salary:80000, address: {line1: 'c/o Unstructured Programmers Anonymous', line2: '7723 Western Avenue', city: 'San Francisco', state: 'CA', zip: '9123'}},"
-				+ "{name: 'Brian Kernighan', salary:60000, address: {line1: 'PO BOX 455', city:'Princeton', state:'NJ', zip: '08544'}}]";
-
-		String sample2 = "[{name: \"Douglas Crawford\", salary:80000, address: {line1: \"c/o Unstructured Programmers Anonymous\", line2: \"7723 Western Avenue\", city: \"San Francisco\", state: \"CA\", zip: \"9123\"}},"
-				+ "{name: \"Brian Kernighan\", salary:60000, address: {line1: \"PO BOX 455\", city:\"Princeton\", state:\"NJ\", zip: \"08544\"}}]";
-
-		String [] s = sample2.split(",");
-		for(String a : s){
-			System.out.println(a);
+		Jsonparser a = new Jsonparser();
+		String[] first = a.first;
+//		for(String b : first){
+//			System.out.println(b);
+//		}
+	}
+	
+	public void mapping(){
+		for(String f : first){
+			String [] second = f.split(":");
 		}
 	}
 }
