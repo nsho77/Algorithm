997f2813c8a76ebd2b07a34c9eff50929f2f1840
Seongho <Seongho@DESKTOP-68QQH5S>
2017-02-12 13:42:56 +0900

    Signed-off-by: Seongho <Seongho@DESKTOP-68QQH5S>

--- Algorithm/src/codility/OddoccurenceInArray.java
+++ Algorithm/src/codility/OddoccurenceInArray.java
@@ -11,22 +11,37 @@ public class OddoccurenceInArray {
 
 	public int solution(int[] A) {
 		// write your code in Java SE 8
-		int answer =0;
-		for(int i=0; i<A.length-1; i++){
-			for(int j=i+1; j<A.length;j++){
-				if(A[i]==A[j]){
-					A[i]=0;
-					A[j]=0;
-				}
-			}
+//		int answer =0;
+//		for(int i=0; i<A.length-1; i++){
+//			for(int j=i+1; j<A.length;j++){
+//				if(A[i]==A[j]){
+//					A[i]=0;
+//					A[j]=0;
+//				}
+//			}
+//		}
+//		for(int a : A){
+//			if(a != 0){
+//				answer =a;
+//				break;
+//			}
+//		}
+//		System.out.println(answer);
+//		return answer;
+		
+		for (int a : A){
+			System.out.print(a);
 		}
-		for(int a : A){
-			if(a != 0){
-				answer =a;
-				break;
-			}
+		System.out.println();
+		int i;
+		int res = 0;
+		for (i = 0; i < A.length; i++) 
+		{
+		res = res ^ A[i]; // Implementation of XOR
+		System.out.println(res);
 		}
-		System.out.println(answer);
-		return answer;
+		//System.out.println(res);
+		return res;
+
 	}
 }
